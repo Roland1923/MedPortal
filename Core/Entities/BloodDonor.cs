fbdf312b5e9c6253f13e5f4da6ada832dfc3ae68
Iordache Roland <roland.iordache96@gmail.com>
2017-11-30 03:52:13 +0200

    Add files via upload

---
+++ Core/Entities/BloodDonor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Core.Entities
+{
+    public class BloodDonor
+    {
+        [Required]
+        public Guid BloodDonorID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string BloodGroup { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Country { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsPrivate { get; set; }
+        public bool IsVerified { get; set; }
+    }
+}
---
+++ Core/IRepositories/IBloodDonorRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Core.Entities;
+
+namespace Core.IRepositories
+{
+    public interface IBloodDonorRepository
+    {
+        void AddBloodDonor(BloodDonor b);
+        void EditBloodDonor(BloodDonor b);
+        void DeleteBloodDonor(Guid id);
+        IReadOnlyCollection<BloodDonor> GetAllBloodDonors();
+        BloodDonor GetBloodDonorById(Guid id);
+    }
+
+}
