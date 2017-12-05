47556a6addb7310979000ea312b5d1e9b2073894
stefanonu <onu.stefan19@yahoo.com>
2017-12-05 18:44:16 +0200

    Add Appointment
    - Entitie
    - Repository
    - Controller ( CRUD )

---
+++ Core/IRepositories/IAppointmentRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Core.Entities;
+
+namespace Core.IRepositories
+{
+    public interface IAppointmentRepository
+    { 
+        void AddAppointment(Appointment patient);
+        void EditAppointment(Appointment patient);
+        void DeleteAppointment(Guid id);
+        IReadOnlyCollection<Appointment> GetAllAppointments();
+        Appointment GetAppointmentById(Guid id);
+    }
+}
---
+++ Tests/IntegrationTests/AppointmentRepositoryTests.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Core.Entities;
+using Infrastructure.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.IntegrationTests
+{
+    [TestClass]
+    public class AppointmentRepositoryTests : BaseIntegrationTests
+    {
+        [TestMethod]
+        public void Given_AppointmentRepository_When_AddingAnAppointment_Then_TheAppointmentShouldBeProperlySaved()
+        {
+            RunOnDatabase(ctx => {
+                //Arrange
+                var repository = new AppointmentRepository(ctx);
+                var patient = Patient.Create("Roland", "Iordache", "roland.iordache96@gmail.com", "Iasi", new DateTime(1996, 02, 10), "0746524459");
+                var appointment = Appointment.Create(new DateTime(1996, 02, 10),patient);
+
+                //Act
+                repository.AddAppointment(appointment);
+
+                //Assert
+                Assert.AreEqual(repository.GetAllAppointments().Count, 1);
+            });
+        }
+        [TestMethod]
+        public void Given_AppointmentRepository_When_DeletingAnAppointment_Then_TheAppointmentShouldBeProperlyRemoved()
+        {
+            RunOnDatabase(ctx => {
+                //Arrange
+                var repository = new AppointmentRepository(ctx);
+                var patient = Patient.Create("Roland", "Iordache", "roland.iordache96@gmail.com", "Iasi", new DateTime(1996, 02, 10), "0746524459");
+                var appointment = Appointment.Create(new DateTime(1996, 02, 10), patient);
+
+                repository.AddAppointment(appointment);
+                var id = appointment.AppointmentId;
+
+                //Act
+                repository.DeleteAppointment(id);
+
+                //Assert
+                Assert.AreEqual(repository.GetAllAppointments().Count, 0);
+            });
+        }
+        [TestMethod]
+        public void Given_AppointmentRepository_When_EditingAnAppointment_Then_TheAppointmentShouldBeProperlyEdited()
+        {
+            RunOnDatabase(ctx => {
+                //Arrange
+                var repository = new AppointmentRepository(ctx);
+                var patient = Patient.Create("Roland", "Iordache", "roland.iordache96@gmail.com", "Iasi", new DateTime(1996, 02, 10), "0746524459");
+                var appointment = Appointment.Create(new DateTime(2017, 12, 04), patient);
+                repository.AddAppointment(appointment);
+                var currentDate = appointment.AppointmentDate;
+                appointment.Update(new DateTime(2017,12,03),patient);
+                var newCurrentDate = appointment.AppointmentDate;
+
+                //Act
+                repository.EditAppointment(appointment);
+
+                //Assert
+                Assert.AreNotEqual(currentDate, newCurrentDate);
+            });
+        }
+        [TestMethod]
+        public void Given_AppointmentRepository_When_ReturningAnAppointment_Then_TheAppointmentShouldBeProperlyReturned()
+        {
+            RunOnDatabase(ctx => {
+                //Arrange
+                var repository = new AppointmentRepository(ctx);
+                var patient = Patient.Create("Roland", "Iordache", "roland.iordache96@gmail.com", "Iasi", new DateTime(1996, 02, 10), "0746524459");
+                var appointment = Appointment.Create(new DateTime(1996, 02, 10), patient);
+                repository.AddAppointment(appointment);
+
+                //Act
+                var extractedAppointment = repository.GetAppointmentById(appointment.AppointmentId);
+
+                //Assert
+                Assert.AreEqual(appointment, extractedAppointment);
+            });
+        }
+        [TestMethod]
+        public void Given_AppointmentRepository_When_ReturningAllAppointments_Then_AllAppointmentsShouldBeProperlyReturned()
+        {
+            RunOnDatabase(ctx => {
+                //Arrange
+                var repository = new AppointmentRepository(ctx);
+                var patient = Patient.Create("Roland", "Iordache", "roland.iordache96@gmail.com", "Iasi", new DateTime(1996, 02, 10), "0746524459");
+                var appointment = Appointment.Create(new DateTime(1996, 02, 10), patient);
+                repository.AddAppointment(appointment);
+
+                //Act
+                var count = repository.GetAllAppointments().Count;
+
+                //Assert
+                Assert.AreEqual(count, 1);
+            });
+        }
+    }
+}
