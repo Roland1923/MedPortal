6a27a983933992d384b77b0dfd1b5ae3c5a19c36
VicTalif <victor.talif@gmail.com>
2017-12-05 23:31:42 +0200

    Add files via upload

---
+++ Core/Entities/BloodType.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Core.Entities
+{
+    public class BloodType
+    {
+        private BloodType() { }
+
+        public static BloodType Create(string type)
+        {
+            var instance = new BloodType { BloodTypeId = Guid.NewGuid(), Patients = new List<Patient>() };
+            instance.Update(type);
+            return instance;
+        }
+
+        public void Update(string type)
+        {
+            Type = type;
+        }
+
+        [Key]
+        [Required]
+        public Guid BloodTypeId { get; private set; }
+
+        [Required(ErrorMessage = "You must provide a valid Blood Type!")]
+        [Display(Name = "Blood Type (O1, A2, B3, AB4)")]
+        [RegularExpression(@"^(01|A2|B3|AB4)$", ErrorMessage = "Not a valid Blood Type!")]
+        public string Type { get; private set; }
+
+        //[Required]
+        //Putem instantia initial empty?
+        public List<Patient> Patients { get; private set; }
+    }
+}
