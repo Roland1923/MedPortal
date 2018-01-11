c65ec281f51ea665bf9e145b32b666b7f155c311
VicTalif <victor.talif@gmail.com>
2018-01-11 07:43:15 +0200

    Add files via upload

--- WebApp/Models/ApiResponse.cs
+++ WebApp/Models/ApiResponse.cs
@@ -9,6 +9,7 @@ namespace WebApp.Models
         public Appointment Appointment { get; set; }
         public BloodDonor BloodDonor { get; set; }
         public Feedback Feedback { get; set; }
+        public BloodBank BloodBank { get; set; }
         public Doctor Doctor { get; set; }
         public Patient Patient { get; set; }
         public PatientHistory PatientHistory { get; set; }
