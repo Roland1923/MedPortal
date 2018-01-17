e84665a69fa446ce1af327068a4e733b1f165a97
stefanonu <onu.stefan19@yahoo.com>
2018-01-17 19:01:46 +0200

    Rutare pagina Home + Login
    - Login
    - rutare fix

--- ClientApp/src/app/header/header.component.ts
+++ ClientApp/src/app/header/header.component.ts
@@ -3,18 +3,24 @@ import { Router } from '@angular/router';
 
 import { AuthComponent } from '../auth/auth.component';
 import { UserService } from '../shared/services/user.service';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../shared/services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  providers : [AuthService]
 })
 export class HeaderComponent implements OnInit {
   showHeader: boolean = false;
   auth: AuthComponent;
   errors : string;
-
-  constructor(private userService : UserService, private router: Router) { }
+  private postStream$: Subscription;
+  private email_login: string;
+  private password_login: string;
+  
+  constructor(private authService: AuthService,private userService : UserService, private router: Router) { }
 
   ngOnInit() {
     this.router.events.subscribe(event => this.modifyHeader(event));
@@ -36,13 +42,23 @@ export class HeaderComponent implements OnInit {
   }
 
   headerLogin() {
-    this.auth.login();
 
-    /* Check if autentification faild and show error */
+      if (this.postStream$) { this.postStream$.unsubscribe }
+   
+      this.postStream$ = this.authService.login$(this.email_login, this.password_login).subscribe(
+          result => {
+              if (result.state == 1) {
+                  this.router.navigate(["home"]);
+              } else {
+                  alert(result.msg);
+              }
+          }
+      )
+  
     if(this.userService.getLogginState() == false) {
       this.errors = "Email sau parola incorecte";
     }
   }
-
 }
+
 //scripturile jos, css-urile sus
